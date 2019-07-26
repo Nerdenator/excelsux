b770316ccbd6026e2699bf6ff82af3b8dcc9e26a
am034402 <aaron.mcruer@cerner.com>
2019-07-26 15:46:10 -0500

    making stuff using desktop excel

--- excel.py
+++ excel.py
@@ -1,25 +1,32 @@
 from openpyxl import load_workbook
 import timeit
+import csv
+import pandas as pd
 
+def read_in_xlsx_openpyxl():
+    try:
+        wb = load_workbook(filename='randoms.xlsx')
+    except Exception as ex:
+        print(ex)
 
-def main():
-    print_everything()
+def read_in_file_csv():
+    with open('randoms.csv') as csvfile:
+        randomreader = csv.reader
 
+def read_in_xls_pandas_python_dataset():
+    df = pd.read_excel('randoms.xls')
 
-def print_everything():
-    wb = load_workbook('Book.xlsx')
-    # get the "active" worksheet, whatever that is (ambiguous)
-    ws = wb.active
-    # you can hardcode it, but what if it's changed? process is broken
-    #tuple_o_values = tuple(ws.values)
-    # that's a bunch of numbers, right? wrong. it's '=RAND()' or None, over and over again.
-    #print(tuple_o_values)
-    for row in ws.values:
-        for value in row:
-            print(value)
-    #print(len(tuple_o_values))
+def read_in_xls_pandas_dataframe():
+    pass
+# def main():
+#
+#     timeit.timeit('read_in_xlsx_openpyxl()')
 
-
-if __name__ == '__main__':
-    main()
-
+# if __name__ == '__main__':
+#     print(timeit.timeit("read_in_file_csv()", setup='from __main__ import read_in_csv_file'))
+print("CSV read-in:")
+print(timeit.timeit(read_in_file_csv, number=1000))
+print("XLSX read-in:")
+print(timeit.timeit(read_in_xlsx_openpyxl, number=1000))
+print("XLS read-in:")
+print(timeit.timeit(read_in_xls_pandas_python_dataset, number=1000))
