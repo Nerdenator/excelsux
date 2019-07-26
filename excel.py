a66870941dc515ba91e5efbee6d77127a1c37aca
am034402 <aaron.mcruer@cerner.com>
2019-07-26 16:26:46 -0500

    needs to be modified do measurements in each function as opposed to in a call

--- excel.py
+++ excel.py
@@ -4,29 +4,33 @@ import csv
 import pandas as pd
 
 def read_in_xlsx_openpyxl():
-    try:
-        wb = load_workbook(filename='randoms.xlsx')
-    except Exception as ex:
-        print(ex)
+    wb = load_workbook(filename='randoms.xlsx')
+    return wb
+
+def read_in_xlsx_pandas():
+    df = pd.read_excel('randoms.xlsx')
+    return df
 
 def read_in_file_csv():
     with open('randoms.csv') as csvfile:
-        randomreader = csv.reader
+        randomreader = csv.reader(csvfile)
+        return randomreader
 
 def read_in_xls_pandas_python_dataset():
     df = pd.read_excel('randoms.xls')
+    return df
 
 def read_in_xls_pandas_dataframe():
     pass
-# def main():
-#
-#     timeit.timeit('read_in_xlsx_openpyxl()')
 
-# if __name__ == '__main__':
-#     print(timeit.timeit("read_in_file_csv()", setup='from __main__ import read_in_csv_file'))
-print("CSV read-in:")
+print("CSV read-in, stdlib csv:")
 print(timeit.timeit(read_in_file_csv, number=1000))
-print("XLSX read-in:")
+csv_reader = read_in_file_csv()
+for row in csv_reader:
+    print(row)
+print("XLSX read-in, openpyxl:")
 print(timeit.timeit(read_in_xlsx_openpyxl, number=1000))
-print("XLS read-in:")
+print("XLS read-in, pandas:")
 print(timeit.timeit(read_in_xls_pandas_python_dataset, number=1000))
+print("XLSX read-in, pandas:")
+print(timeit.timeit(read_in_xlsx_pandas, number=1000))
